47ffdba7f56a60cb0dc6b996d9fb5170386cae46
Janaina Almeida <40438679+janainaalmeida@users.noreply.github.com>
2022-03-28 23:14:28 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ProjetoElevador.CONTROLLER;
+
+namespace Projeto_Elevador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var controllerElevador = new ControllerElevador();
+
+            controllerElevador.Inicializando();
+        }
+    }
+}
